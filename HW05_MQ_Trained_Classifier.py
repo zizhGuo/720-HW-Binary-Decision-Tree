25aefda99e27b50db55832bbf9daf0bd0164fbd1
Qianjx <jq3513@rit.edu>
2020-03-08 21:51:23 -0400

    final version

---
+++ HW05_MQ_Trained_Classifier.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import numpy as np
+import sys
+def main():
+	test_data_path=sys.argv[1]
+	test_data=pd.read_csv(test_data_path)
+	category_pre=[]
+	for index,row in test_data.iterrows():
+		if(row['BangLn'])>6.0:
+			category_pre.append('Assam')
+		elif(row['Ht'])<=140.0:
+			category_pre.append('Bhuttan')
+		elif(row['EarLobes'])<=0:
+			category_pre.append('Bhuttan')
+		elif(row['TailLn'])<=12.0:
+			category_pre.append('Assam')
+		elif(row['BangLn'])<=5.0:
+			category_pre.append('Bhuttan')
+		else: 
+			category_pre.append('Assam')
+	df=pd.DataFrame(category_pre)
+	df.to_csv("./HW05_MQ_MyClassifications.csv")
+if __name__ == "__main__":
+	main()
