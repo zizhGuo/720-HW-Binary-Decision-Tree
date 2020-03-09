2bd1c13b99bb2b4945d105a445f62a0d2778e097
Qianjx <jq3513@rit.edu>
2020-03-08 21:48:57 -0400

    final version

--- HW05_MQ_Mentor.py
+++ HW05_MQ_Mentor.py
@@ -23,7 +23,7 @@ import pandas as pd # data analysis and manipulation package
 import matplotlib.pyplot as plt # plotting package
 from datetime import datetime # python std data package
 import sys # for using command line argument
-import math
+import math # for entropy calculation
 
 
 class dt_node:
@@ -38,14 +38,15 @@ class dt_node:
         @leaf: a str that represents which side is the leaf node
         @category: a boolean that indicates the category of the leaf node
     """
-    def __init__(self, attribute = None, threshold = None, loss = None, left_node = None, right_node = None, dataframe = None):
+    def __init__(self, attribute = None, threshold = None, loss = None,\
+         left_node = None, right_node = None, dataframe = None):
         self.attribute = attribute
         self.threshold = threshold
         self.loss = loss
         self.left_node = left_node 
         self.right_node = right_node
         self.dataframe = dataframe
-
+        
         if(left_node == None and right_node == None):
             data_leaf=dataframe[dataframe[attribute] <= threshold]
             dataframe_assam = data_leaf[data_leaf['Class'] == 'Assam']
@@ -71,21 +72,32 @@ class dt_node:
             self.leaf = "right"
             self.category = assam_count>bhuttan_count
 
-# the function for testing the node
-def pre_traverse(node):
+
+def pre_traverse_(node):
+    ''' pre traverse the tree, from the root
+
+    '''
     print('attribute:'+str(node.attribute))
     print('threshold:'+ str(node.threshold))
     print('loss:'+str(node.loss))
+    print('size:'+str(np.size(node.dataframe['Age'])))
     print('leaf node:'+str(node.leaf))
     print('category==Assam:'+str(node.category)+'\n')
 
     if node.left_node != None:
-        pre_traverse(node.left_node)
+        pre_traverse_(node.left_node)
     
     if node.right_node != None: 
-        pre_traverse(node.right_node)
+        pre_traverse_(node.right_node)
 
 def pre_traverse(node,tree_body,if_flag):
+    ''' pre traverse the tree
+        used for generating trained program
+    '''
+
+    ''' doing all decisions of parameters
+
+    '''
     if(node.leaf=='right'):
         symbol='>'
     else: symbol='<='
@@ -96,7 +108,8 @@ def pre_traverse(node,tree_body,if_flag):
         if_str='if'
     else: if_str='elif'
 
-    tree_body+='\t\t'+if_str+"(row['"+str(node.attribute)+"'])"+symbol+str(node.threshold)+':\n'
+    tree_body+='\t\t'+if_str+"(row['"+str(node.attribute)+"'])"+\
+        symbol+str(node.threshold)+':\n'
     tree_body+="\t\t\tcategory_pre.append('"+category+"')\n"
 
     if node.left_node != None:
@@ -105,6 +118,9 @@ def pre_traverse(node,tree_body,if_flag):
     if node.right_node != None: 
         tree_body=pre_traverse(node.right_node, tree_body, False)
 
+    ''' special case for the last node
+
+    '''
     if(node.leaf=='last_node'):
         if(node.category):
             category='Bhuttan'
@@ -123,9 +139,8 @@ def data_preprocessing(dataframe):
     """
     columns_size = np.size(dataframe.columns)
     columns = dataframe.columns[0 : columns_size - 1]
-    # print(columns)
+
     for column in columns:
-        # print(column)
         data = np.round(dataframe[column])
         dataframe[column] = data
     
@@ -143,29 +158,41 @@ def lost_function(dataframe, attribute, threshold):
         @dataframe: a dataframe of quantized data
         @attribute: a string of the chosen attribute name
         @threshold: a float type of chosen threshold
+    Returns:
+        @Lost Function: cost function we get
+        @split rate: evalating how unbalanced the split is
 
     """
-    df_left = dataframe[dataframe[attribute] <= threshold]      # left sliced dataframe (<= threshold) 
-    df_right = dataframe[dataframe[attribute] > threshold]      # right sliced dataframe (> threshold)
+
+    # left sliced dataframe (<= threshold) 
+    df_left = dataframe[dataframe[attribute] <= threshold]
+    # right sliced dataframe (> threshold)      
+    df_right = dataframe[dataframe[attribute] > threshold]
+     
     split_rate = np.abs((np.size(df_left[attribute]) \
                         - np.size(df_right[attribute]))) \
                         / np.size(dataframe[attribute])
     df_left_Assam = df_left[df_left['Class'] == 'Assam']
     df_right_Assam = df_right[df_right['Class'] == 'Assam']
 
-    pa_left = np.size(df_left_Assam[attribute]) / np.size(df_left[attribute])
-    pa_right = np.size(df_right_Assam[attribute]) / np.size(df_right[attribute])
-
-    if((pa_left < 0.8 and pa_right < 0.8)and(pa_left > 0.2 and pa_right > 0.2)  ): # <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+    pa_left = np.size(df_left_Assam[attribute]) / \
+                    np.size(df_left[attribute])
+    pa_right = np.size(df_right_Assam[attribute]) / \
+                    np.size(df_right[attribute])
+    
+    # meeting the requirements for accuracy
+    if((pa_left < 0.8 and pa_right < 0.8)and(pa_left > 0.2 and pa_right > 0.2)):
         return 10,split_rate
-
+    
+    # calculate cost function
     Regularization = split_rate
     ObjectiveFunction=min(min(1- pa_left, 1- pa_right),min(pa_right, pa_left))
-    return ObjectiveFunction + 2*Regularization, split_rate
+    return (ObjectiveFunction + 2*Regularization)/3, split_rate
 
 def entropy_average(dataframe, attribute, threshold):
     """ Average weighted Purity calculation for one chosen attribute
-        and threshold
+        and threshold, this function is not used due to the change of 
+        cost function  
     Paras:
         @dataframe: a dataframe of quantized data
         @attribute: a string of the chosen attribute name
@@ -173,8 +200,10 @@ def entropy_average(dataframe, attribute, threshold):
     Return:
         dataframe: a dataframe of data
     """
-    df_left = dataframe[dataframe[attribute] <= threshold]      # left sliced dataframe (<= threshold) 
-    df_right = dataframe[dataframe[attribute] > threshold]      # right sliced dataframe (> threshold)
+    # left sliced dataframe (<= threshold) 
+    df_left = dataframe[dataframe[attribute] <= threshold] 
+    # right sliced dataframe (> threshold)    
+    df_right = dataframe[dataframe[attribute] > threshold]      
     split_rate = np.abs((np.size(df_left[attribute]) \
                         - np.size(df_right[attribute]))) \
                         / np.size(dataframe[attribute])
@@ -184,30 +213,18 @@ def entropy_average(dataframe, attribute, threshold):
     p_left = np.size(df_left_Assam[attribute]) / np.size(df_left[attribute])
     p_right = np.size(df_right_Assam[attribute]) / np.size(df_right[attribute])
     
-    # print('  threshold = ' + str(threshold))
-    # print('  p_left_assum = ' + str(p_left) )
-    # print('  p_right_assum = ' + str(p_right) )
-    # print('  ' + str(split_rate))
-    # print("")
-    # print("p_left = " + str(p_left))
-    # print("p_right = " + str(p_right))
-  
     ent_left =  0 if p_left == 0 or p_left == 1 \
                 else - p_left * math.log(p_left, 2) \
                     - (1 - p_left) * math.log ((1 - p_left), 2)
     ent_right = 0 if p_right == 0 or p_right == 1 \
                 else - p_right * math.log(p_right, 2) \
                     - (1 - p_right) * math.log ((1 - p_right), 2)
-  
-    # print("ent_left = " + str(ent_left))
-    # print("ent_right = " + str(ent_right))
 
     weight_left = np.size(df_left['Age']) / np.size(dataframe['Age'])
     weight_right =  1 - weight_left
-    ent_average = ent_left * weight_left + ent_right * weight_right
 
-    # print("ent_average = " + str(ent_average) )
-    # print("")
+    # getting entropy of the split
+    ent_average = ent_left * weight_left + ent_right * weight_right
 
     return ent_average, split_rate
 
@@ -215,10 +232,11 @@ def threshold_selection_in_attribute(dataframe, attribute, step):
     min_threshold = np.min(dataframe[attribute]) # starting threshold 
     max_threshold = np.max(dataframe[attribute]) # ending threshold
 
-    best_threshold = min_threshold # best threshold that provides minimized entropy
+    # best threshold that provides minimized entropy
+    best_threshold = min_threshold 
     best_loss = np.inf # the minimized average entropy
     threshold = min_threshold # threshold for traversing
-    best_split_rate = np.inf
+    best_split_rate = np.inf # best split rate
 
     while threshold < max_threshold:
         
@@ -234,38 +252,36 @@ def threshold_selection_in_attribute(dataframe, attribute, step):
             best_split_rate = split_rate
         
         threshold += step
-    # print("attribute = " + attribute)
-    # print("best ent_average = " + str(best_loss))
-    # print("best threshold = " + str(best_threshold))
-    # print("best split rate = " + str(best_split_rate))
-    # print("")
+    
     return attribute, best_threshold, best_loss, best_split_rate
 
 def class_assign(dataframe):
+    # a printing function for all leaf node we get
     dataframe_assam = dataframe[dataframe['Class'] == 'Assam']
     dataframe_bhuttan = dataframe[dataframe['Class'] == 'Bhuttan']
     assam_count = np.size(dataframe_assam['Age'])
     bhuttan_count = np.size(dataframe_bhuttan['Age'])
     
-    print("Class: Bhuttan : "+ str(bhuttan_count))
     print("Class: Assam: " + str(assam_count))
+    print("Class: Bhuttan : "+ str(bhuttan_count))
+    
 
 def decision_tree(dataframe, depth):
     # ---------------- Stop Criteria--------------------------------
     df_Assam = dataframe[dataframe['Class'] == 'Assam']
     class_rate = np.abs(np.size(df_Assam['Age']) / np.size(dataframe['Age']))
     if  class_rate > 0.8 or class_rate < 0.2:
-        print('case 2')
+        print('leaf node')
         class_assign(dataframe)
         return None
 
     if np.size(dataframe['Age']) < 15:
-        print('case 1')
+        print('leaf node')
         class_assign(dataframe)
         return None
     
     if depth >= 8:
-        print('case 3')
+        print('leaf node generated:')
         class_assign(dataframe)
         return None
  
@@ -277,7 +293,7 @@ def decision_tree(dataframe, depth):
     best_split_rate = np.inf
 
     for attribute in dataframe.columns[0 : np.size(attributes) - 1]:
-        # print(attribute)
+        # attributes selection and threshold testing
         step = 1.0
         if attribute == 'Age': step = 2.0
         if attribute == 'Ht': step = 5.0
@@ -289,19 +305,16 @@ def decision_tree(dataframe, depth):
             best_loss = loss
             best_split_rate = split_rate
 
-    # print("--------------------------------------------")
-    # print("best attribute = " + best_attribute)
-    # print("best threshold = " + str(best_threshold))
-    # print("best entropy_average = " + str(best_ent_average))
-    # print("best spli rate =  " + str(best_split_rate))
+    # ------Create current Node and recursively create child nodes-------
 
-    # ----------------Create current Node and recursively create child nodes--------------------------------
+    left_node = decision_tree(dataframe[dataframe[best_attribute] \
+                                <= best_threshold], depth + 1)
+    right_node = decision_tree(dataframe[dataframe[best_attribute] \
+                                > best_threshold], depth + 1)
 
-    left_node = decision_tree(dataframe[dataframe[best_attribute] <= best_threshold], depth + 1)
-    right_node = decision_tree(dataframe[dataframe[best_attribute] > best_threshold], depth + 1)
+    current_node = dt_node(best_attribute, best_threshold, best_loss, \
+                                left_node, right_node, dataframe)
 
-    current_node = dt_node(best_attribute, best_threshold, best_loss, left_node, right_node, dataframe)
-    # print(depth)
     return current_node
 
 def trained_program_gen(root):
@@ -314,21 +327,27 @@ category_pre=[]\n'
 
     tails=\
 '\tdf=pd.DataFrame(category_pre)\n\t\
-df.to_csv("./output.csv")\n\
+df.to_csv("./HW05_MQ_MyClassifications.csv")\n\
 if __name__ == "__main__":\n\t\
 main()'
     
     tree_body='\tfor index,row in test_data.iterrows():\n'
     tree_body=pre_traverse(root,tree_body,if_flag=True)
+    # generating codes
     return(headers+tree_body+tails)
 
 def main():
-    df_snowfolks_data_raw = pd.read_csv('720-HW-Binary-Decision-Tree\Abominable_Data_HW05_v720.csv')
+    # read_csv
+    df_snowfolks_data_raw = pd.read_csv(sys.argv[1])
+    # quantized data
     df_snowfolks_data_quantized = data_preprocessing(df_snowfolks_data_raw)
-
+    # tree generating
     root = decision_tree(df_snowfolks_data_quantized, 0)
-    
-    trained_program=open('trained_program.py','w')
+    # show results
+    print('tree structure')
+    pre_traverse_(root)
+    # trained_program_gen
+    trained_program=open('./HW05_MQ_Trained_Classifier.py','w')
     trained_program.write(trained_program_gen(root))
 
 if __name__ == "__main__":
--- Report.md
+++ Report.md
@@ -1,8 +1,39 @@
-a) Describe the structure of your final trained classifier program (the resulting classifier).
+## HW05's Report
+
+### a) What were your names?
+Martin Qian and Zizhun Guo
+
+### b) Who did what roles during the assignment?
+**Zizhun Guo:**
+1 - decision node data structure
+2 - entropy calculation function
+3 - cost function for using average entropy
+3 - split rate function
+4 - decision make function (recursion)
+5 - report 
+
+**Martin Qian:**
+1 - Cost function modification for new requirment (misclassification + spit rate)
+2 - Decision tree parameter testing
+3 - trained file writting function
+4 - csv file writting function
+5 - report 
+
+### c) What cost function did you design for your splitting criterion?
+How did you design it?
+What was your objective function?
+What part was your regularization?
+
+This is our main design part. That is: 
+$Cost = (misclassficationRate + 2 * splitRate)/3$
+See conclusion part to see why we choose this.
+
+## d) Describe the structure of your final trained classifier program (the resulting classifier).
+What does it tell you about the relative importance of the attributes?
 
 ```python 
     for row in test_data:
-        if(row['BangLn']>= 6.0):
+        if(row['BangLn']> 6.0):
             category_pre.append('Assam')
         elif(row['Ht']<= 140):
             category_pre.append('Bhuttan')
@@ -10,20 +41,17 @@ a) Describe the structure of your final trained classifier program (the resultin
             category_pre.append('Bhuttan')
         elif(row['TailLn']<= 12.0):
             category_pre.append('Assam')
-        elif(row['BangLn']>= 5.0):
-            category_pre.append('Assam')
-        else:
+        elif(row['BangLn']<= 5.0):
             category_pre.append('Bhuttan')
+        else:
+            category_pre.append('Assam')
 
 ```
 
 
-What does it tell you about the relative importance of the attributes?
-
 The closer to the root decision node is, the heavier the importance is. Since the root decision node has the minimal cost than other nodes, so does the second minimal, the third monimal and so forth.
 
-
-b) Generate a confusion matrix for the given training data:
+### c) Generate a confusion matrix for the given training data:
 ||True|False|
 |---|---|---|
 |Assam|347|53|
@@ -39,48 +67,35 @@ How many Bhutan were classified as Assam?
 How many Bhutan were classified as Bhutan?
 351
 
-c) What was the accuracy of your resulting classifier, on the training data?
+### d) What was the accuracy of your resulting classifier, on the training data?
 $(799 - 101) / 799 = 0.8736 $
 
-d) What was the hardest part of getting all this working?
+### e) What was the hardest part of getting all this working?
 
 The most difficult part is to balance the "pure" favors and "big" favors for cost function. It is hard to define which alpha value is the one that can produce the decision tree with highest accuracy. 
 
 
-e) Did anything go wrong?
-This answers links to the previous question. When using 0.95 as the accuracy for one of the stop criterias, the last tree would still have about 600 data records that has not yet been classified. The solution for this issue is written in conclusion part.
+### f) Did anything go wrong?
+This answers links to the previous question. When using 0.95 as the accuracy for one of the stop criterias, the last tree node would still have about 600 data records that has cannot be classified properly, which lead to really bad results.
 
-f) Discussion:
-What does the number 23 have to do with anything?
-What does this have to do with math, statistics, or decision making?
-Or, did the professor pull this number out of a hat?
+### g) Discussion:
+What would happen if you changed the number 15 to 45?
+What if it was 5?
+What does this value control?
 
+This value stands for the minimum number of records inside one node. It is used to determine when the recursion of decision tree would stop(It has the similar function of limit for depth. However, depth tells us what the highest depth is, this number tells us what when you can stop in advance if not reaching the highest depth). In my case, the second last node contains 56 element so chaning from 15 to 45 will make no difference. 
 
+Changing from 15 to 5 will make the decision tree a little bigger, which means it has more depth.
 
-g) Conclusions
+This value helps determine when to stop, so it controls the depth of the tree.
 
-#### Contribution:
-**Zizhun Guo:**
-1 - decision node data structure
-2 - entropy calculation function
-3 - cost function for using average entropy
-3 - split rate function
-4 - decision make function (recursion)
-5 - report writting
-6 - code comments
+### h) Conclusions
 
-**Martin Qian:**
-1- Cost function modification for new requirment (misclassification + spit rate)
-2 - Decision tree making testing
-3 - trained file writting function
-4 - csv file writting function
-
-
-### Why use misclassification?
+#### Why use misclassification?
 We have used two impurity measures as the object function: weighted entropy and misclassification.
-It is more intuitive by using misclassification.
+To build a fast-decision cascade, the idea itself is based on correctness so it is more intuitive by using misclassification than entropy. Also, entropy is more complex and has little more impact than misclassification rate for this question.
 
-### **Decision Tree design decisions**
+#### **Decision Tree design decisions**
 In order to find out the decision node for each level of decision tree, we assign a COST function (Cost = object function + regularization) to determines the attribute and its corresponding threshold. Therefore, the cost function is expressed in following:
 
 $Cost = misclassficationRate + 2 * splitRate$
@@ -91,25 +106,24 @@ $Cost = misclassficationRate + 2 * splitRate$
 stop criteria =  accuracy 0.95 for misclassification for splitting leaf node.
 > all size of node sliced dataframe is small except for the last node. (around 600 records left)
 > The reason is the tree depth is limitted to 8.
-> last node, impurity is big, which if it is used for determine the class for the final round, it has small accuray for the majority of the records in dataset.
+> For last node, impurity is very high, which if it is used for determine the class for the final round, it has small accuray and it is the majority of the records in dataset.
 
 #### Design decision 2: Mix "pure" and "big"
 > In order to solve this problem, decides to adjust the porportion by favoring "big" number of records. The approach is to increase the weight of regualrtion portion within the cost function by assign a weight in value of 2. 
-> The results improves a bit. However, it still cannot satisify the exptation to have records distribute evenly along with the tree nodes. 
-> Even after with multiple tries by increasing the portaion of rgulartion, it still hurts the model. 
-> This proves that the "pure" parameters has issue, so that all tested adjusted "big" parameters contributes less if only considering ajust split rates. *?*
+> The results improves a bit. However, it still cannot satisify the expectation to have records distribute evenly along with the tree nodes. 
+> Even after with multiple tries by increasing the portaion of rgulartion, the model still has the same problem. 
 
 #### Design decision 3: Mix still but adjust "pure" weight
 > So in order to make improvement, it is necessary to modify the stop criteria.
-> Based on the experiments by having the value of stop criteria tested in value range from 0.5 to 0.9 (step equals to 0.5), it was found that 0.8 is the best stop criteria.   
+> Based on the experiments by having the value of stop criteria tested in value range from 0.7 to 0.9 (step equals to 0.05), it was found that 0.8 is the best stop criteria, which gives us best overall misclassification rate.   
 
 
-###**Underfitting vs Overfitting**
+#### **Underfitting vs Overfitting**
 
 **Case 1: Decision tree becomes OneR model**
-In the step of experiementing adjusting the stop criteria value, we found when the value equals to 0.7, the decision tree becomes in OneR model. This implys the tree is underfitting comparing to what was created when the value euqals to 0.8.
+>In the step of experiementing adjusting the stop criteria value, we found when the value equals to 0.7, the decision tree becomes in OneR model. This implys the tree is underfitting comparing to what was created when the value euqals to 0.8.
 
 **Case 2: One more level of depth whereas accuracy unchanged**
-However, when the stop criteria switched to 0.85, by having only one more decision node different with the tree created in 0.8, it became overfitting. 
+>However, when the stop criteria switched to 0.85, the model will do one more decision than that of 0.8. However, by comparing them we found the first 5 decisions made by them is all the same and model formed by 0.85 is simply doing another decision inside the last node of model of 0.8 and the total misclassifaction rate remained the same. So this suggests overfitting. 
 
 
